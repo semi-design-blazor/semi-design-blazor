766aae00933ccfe157b740795456aeb0917f8928
token <239573049@qq.com>
2022-12-27 00:42:37 +0800

    优化iconbutton

--- src/Component/BlazorComponent/Components/Button/IconButton.razor.cs
+++ src/Component/BlazorComponent/Components/Button/IconButton.razor.cs
@@ -23,7 +23,13 @@ public partial class IconButton
     [Parameter]
     public string[]? NoHorizontalPadding { get; set; }
 
-    public string PrefixCls { get; set; } = "semi-icon";
+    public bool SemiChildrenAlias { get; set; }
+
+    public ComponentCssProvider BtnTextCls { get; } = new();
+
+    public string? XSemiProp { get; set; }
+
+    private string PrefixCls { get; set; } = "semi-icon";
 
     protected override void OnInitialized()
     {
@@ -38,7 +44,48 @@ public partial class IconButton
 
         if (NoHorizontalPadding?.Length > 1)
         {
-            
+            if (NoHorizontalPadding.Any(x => x == "left"))
+            {
+                CssProvider.StyleApply("padding-left:0px");
+            }
+            if (NoHorizontalPadding.Any(x => x == "right"))
+            {
+                CssProvider.StyleApply("padding-right:0px");
+            }
+        }
+        if (NoHorizontalPadding?.Length == 1)
+        {
+            if (NoHorizontalPadding.Any(x => x == "true"))
+            {
+                CssProvider.StyleApply("padding-left:0px");
+                CssProvider.StyleApply("padding-right:0px");
+            }
+            else if (NoHorizontalPadding.Any(x => x == "left"))
+            {
+                CssProvider.StyleApply("padding-left:0px");
+            }
+            else if (NoHorizontalPadding.Any(x => x == "right"))
+            {
+                CssProvider.StyleApply("padding-right:0px");
+            }
+        }
+
+        if (SemiChildrenAlias)
+        {
+            XSemiProp = "x-semi-children-alias";
+        }
+        else
+        {
+            XSemiProp = "children";
+        }
+
+        if (ChildContent == null)
+        {
+            CssProvider.CssApply(PrefixCls + "-with-icon-only");
+        }
+        if (Loading)
+        {
+            CssProvider.CssApply(PrefixCls + "-loading");
         }
 
         base.OnInitialized();
